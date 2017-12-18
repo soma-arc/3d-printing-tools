38f1f06247d8dbb8f336f5e6ba6f1bf861244267
soma_arc <somaarcr@gmail.com>
2017-12-18 22:00:07 +0900

    Add premake file

---
+++ GenTex/premake5.lua
@@ -0,0 +1,37 @@
+sources = {
+   "src/main.cpp",
+}
+
+-- premake5.lua
+workspace "GenTexWorkspace"
+   configurations { "Release", "Debug" }
+
+   platforms { "native", "x64", "x32" }
+
+   -- Use c++11
+   flags { "c++11" }
+
+   includedirs { "./include" }
+
+   includedirs { "./src" }
+
+   -- A project defines one build target
+   project "GenTex"
+      kind "ConsoleApp"
+      language "C++"
+      files { sources }
+
+      if os.is("Linux") then
+         links { "GL", "glfw" }
+      end
+
+      configuration "Debug"
+         defines { "DEBUG" } -- -DDEBUG
+         symbols "On"
+         targetname "genMesh_debug"
+
+      configuration "Release"
+         -- defines { "NDEBUG" } -- -NDEBUG
+         symbols "On"
+         flags { "Optimize" }
+         targetname "genTex"
